577390aaa07231750c8200649c089e71d962f56b
Andrey Lebedev <andrey@lebedev.lt>
2012-12-01 20:07:43 +0200

    Basic order logic implemented and tested

--- src/soaringcoupons/model.py
+++ src/soaringcoupons/model.py
@@ -1,5 +1,7 @@
 from __future__ import absolute_import
 
+import datetime
+
 from google.appengine.ext import db
 
 class CouponType(object):
@@ -29,6 +31,8 @@ def get_coupon_type(id):
 
 
 class Settings(db.Model):
+    """ System settings
+    """
     webtopay_project_id = db.StringProperty(required=False, default='')
     webtopay_password = db.StringProperty(required=False, default='')
     debug = db.BooleanProperty(required=True, default=True)
@@ -46,3 +50,108 @@ def get_settings():
                 for key in Settings.properties().keys()}
     settings['configured'] = entity.is_configured()
     return settings
+
+
+class Order(db.Model):
+    ST_PENDING = 1
+    ST_PAID = 2
+    ST_CANCELLED = 3
+
+    coupon_type = db.StringProperty(required=True)
+    quantity = db.IntegerProperty(required=True, default=1)
+    price = db.FloatProperty(required=True)
+    currency = db.StringProperty(required=True, default='LTL')
+    paid_amount = db.FloatProperty()
+    paid_currency = db.StringProperty()
+    payer_name = db.StringProperty()
+    payer_surname = db.StringProperty()
+    payer_email = db.EmailProperty()
+    payment_provider = db.StringProperty()
+    test = db.BooleanProperty(required=True, default=False)
+    status = db.IntegerProperty(required=True, default=ST_PENDING,
+                               choices=set([ST_PENDING, ST_PAID, ST_CANCELLED]))
+    create_time = db.DateTimeProperty(required=True)
+    payment_time = db.DateTimeProperty()
+
+    @property
+    def order_id(self):
+        return self.key().name()
+
+def order_gen_id():
+    """Generate unique order id.
+    """
+    key = db.Key.from_path('Order', 1)
+    batch = db.allocate_ids(key, 1)
+    return str(batch[0])
+
+@db.transactional
+def order_create(order_id, coupon_type, test=False):
+    order = Order(key_name=order_id,
+                  coupon_type=coupon_type.id,
+                  price=coupon_type.price,
+                  currency='LTL',
+                  test=test,
+                  create_time = datetime.datetime.now(),
+                  status=Order.ST_PENDING)
+    db.put(order)
+    return order
+
+@db.transactional
+def order_cancel(order_id):
+    """ Cancel order
+
+    Order status is set to ST_CANCELLED.
+    """
+    order = Order.get_by_key_name(order_id)
+    if order.status != Order.ST_PENDING:
+        raise ValueError("Cannot cancel non-pending order %s" % order_id)
+    order.status = Order.ST_CANCELLED
+    db.put(order)
+    return order
+
+@db.transactional
+def order_process(order_id, payer_email,
+                  paid_amount, paid_currency,
+                  payer_name=None, payer_surname=None, payment_provider=None):
+    """ Process order payment.
+
+    Updates order with supplied information and updates status to ST_PAID.
+    Creates Coupon object.  Payment information must be validated before
+    passing to this method.
+
+    Returns pair: order, coupon
+    """
+    order = Order.get_by_key_name(order_id)
+    if order.status != Order.ST_PENDING:
+        raise ValueError("Cannot process non-pending order %s" % order_id)
+
+    order.paid_amount = paid_amount
+    order.paid_currency = paid_currency
+    order.payer_email = payer_email
+    order.payer_name = payer_name
+    order.payer_surname = payer_surname
+    order.status = Order.ST_PAID
+    order.payment_time = datetime.datetime.now()
+    db.put_async(order)
+
+    # create coupon
+    assert order.quantity == 1
+    coupon = coupon_create(order)
+
+    return order, coupon
+
+class Coupon(db.Model):
+    ST_ACTIVE = 1
+    ST_USED = 2
+    status = db.IntegerProperty(required=True, default=ST_ACTIVE,
+                                choices=set([ST_ACTIVE, ST_USED]))
+    use_time = db.DateTimeProperty()
+
+    @property
+    def order(self):
+        return self.parent()
+
+def coupon_create(order):
+    coupon = Coupon(parent=order, key_name=order.order_id)
+    db.put_async(coupon)
+    return coupon
---
+++ src/soaringcoupons/tests/test_model.py
@@ -0,0 +1,128 @@
+# -*- coding: utf-8 -*-
+
+import doctest
+
+from google.appengine.ext import testbed
+
+from soaringcoupons import model
+
+def doctest_order_create():
+    """
+        >>> ct = model.CouponType('test', 300.0, "Test flight")
+        >>> order = model.order_create(model.order_gen_id(), ct, test=True)
+
+        >>> order.coupon_type
+        'test'
+        >>> order.price
+        300.0
+        >>> order.status == model.Order.ST_PENDING
+        True
+        >>> order.currency
+        'LTL'
+        >>> order.create_time
+        datetime.datetime(...)
+    """
+
+def doctest_order_cancel():
+    """
+    Create order
+
+        >>> ct = model.CouponType('test', 300.0, "Test flight")
+        >>> order = model.order_create(model.order_gen_id(), ct, test=True)
+
+    Cancelling order changes its status
+        >>> cancelled = model.order_cancel(order.key().name())
+        >>> cancelled.status == model.Order.ST_CANCELLED
+        True
+
+    You cannot cancel already cancelled order
+        >>> model.order_cancel(order.key().name())
+        Traceback (most recent call last):
+        ...
+        ValueError: Cannot cancel non-pending order ...
+
+"""
+
+def doctest_order_process():
+    """
+    Create sample order
+
+        >>> ct = model.CouponType('test', 300.0, "Test flight")
+        >>> order = model.order_create('1', ct, test=True)
+
+    Process successful payment
+
+        >>> order, coupon = model.order_process(order.order_id,
+        ...                                     'test@test.com', 100.0, 'EUR',
+        ...                                     payer_name='Andrey',
+        ...                                     payer_surname='Lebedev',
+        ...                                     payment_provider='dnb')
+        >>> order.status == model.Order.ST_PAID
+        True
+        >>> order.payer_name
+        'Andrey'
+        >>> order.payer_surname
+        'Lebedev'
+        >>> order.payer_email
+        u'test@test.com'
+        >>> order.payment_time != None
+        True
+        >>> order.paid_amount
+        100.0
+        >>> order.paid_currency
+        'EUR'
+
+    Check created coupon
+        >>> coupon.parent() == order
+        True
+        >>> coupon.status == model.Coupon.ST_ACTIVE
+        True
+        >>> coupon.use_time
+        >>> coupon.key().name()
+        u'1'
+
+    Make sure coupon is in database
+        >>> model.Coupon.all().count()
+        1
+    """
+
+def doctest_order_process_twice():
+    """
+    Create sample order
+
+        >>> ct = model.CouponType('test', 300.0, "Test flight")
+        >>> order = model.order_create('2', ct, test=True)
+
+    Process successful payment
+
+        >>> order, coupon = model.order_process(order.order_id,
+        ...                                     'test@test.com', 100.0, 'EUR',
+        ...                                     payer_name='Andrey',
+        ...                                     payer_surname='Lebedev',
+        ...                                     payment_provider='dnb')
+
+    Attempt to process payment second time and expect the exception
+        >>> model.order_process(order.order_id, 'test@test.com', 100.0, 'EUR')
+        Traceback (most recent call last):
+        ...
+        ValueError: Cannot process non-pending order ...
+
+
+    """
+
+def setUp(test):
+    test.testbed = testbed.Testbed()
+    test.testbed.activate()
+    test.testbed.init_datastore_v3_stub()
+
+def tearDown(test):
+    test.testbed.deactivate()
+
+DOCTEST_OPTION_FLAGS = (doctest.NORMALIZE_WHITESPACE|
+                        doctest.ELLIPSIS|
+                        doctest.REPORT_ONLY_FIRST_FAILURE|
+                        doctest.REPORT_NDIFF
+                        )
+def test_suite():
+    return doctest.DocTestSuite(setUp=setUp, tearDown=tearDown,
+                                optionflags=DOCTEST_OPTION_FLAGS)
