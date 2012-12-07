06386962779155b99fa4ce6a2d0f3d09d1b93d08
Andrey Lebedev <andrey@lebedev.lt>
2012-12-08 01:43:56 +0200

    Debugging functions added

---
+++ src/soaringcoupons/debug.py
@@ -0,0 +1,22 @@
+""" Functions for debugging through Interactive Console
+
+To call one of the functions, defined below, enter into the Interactive
+console::
+
+    from soaringcoupons import debug
+
+    debug.function()
+"""
+
+from soaringcoupons import model
+
+def create_coupon():
+    """Create sample order and process it
+    """
+    oid = model.order_gen_id()
+    ct = model.get_coupon_type('acro')
+    model.order_create(oid, ct, test=True)
+    model.order_process(oid, 'test@test.com', 300.0, 'LTL')
+
+    print "Coupon %s created" % oid
+    print "See http://localhost:8080/coupon/%s" % oid
