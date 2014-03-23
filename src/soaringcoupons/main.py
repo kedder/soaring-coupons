7b48d179d456d2ddf67016ef86beebf93d5cc6ba
Andrey Lebedev <andrey@lebedev.lt>
2014-03-23 23:28:07 +0200

    Application startup tested

--- src/soaringcoupons/main.py
+++ src/soaringcoupons/main.py
@@ -4,12 +4,18 @@ import webapp2
 from soaringcoupons import controller
 from soaringcoupons import model
 
-version, ts = os.environ['CURRENT_VERSION_ID'].split('.')
-settings = model.get_settings(version)
-if not settings['configured']:
-    routes = [webapp2.Route(r'/', controller.UnconfiguredHandler, name='home')]
-else:
-    routes = controller.get_routes()
 
-app = webapp2.WSGIApplication(routes=routes, debug=settings['debug'],
-                              config=settings)
+def make_app():
+    cvid = os.environ['CURRENT_VERSION_ID']
+    version, ts = cvid.split('.') if '.' in cvid else ('test', '0')
+    settings = model.get_settings(version)
+    if not settings['configured']:
+        routes = [webapp2.Route(r'/',
+                                controller.UnconfiguredHandler, name='home')]
+    else:
+        routes = controller.get_routes()
+
+    return webapp2.WSGIApplication(routes=routes, debug=settings['debug'],
+                                   config=settings)
+
+app = make_app()
---
+++ src/soaringcoupons/tests/test_main.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+from google.appengine.ext import testbed
+
+from soaringcoupons import model, controller
+
+
+class MainTest(unittest.TestCase):
+    def setUp(self):
+        self.testbed = testbed.Testbed()
+        self.testbed.activate()
+        self.testbed.init_datastore_v3_stub()
+
+    def tearDown(self):
+        self.testbed.deactivate()
+
+    def test_initial_run(self):
+        # On the first startup, application is unconfigured, so only one
+        # handler is present, that should guide the configuration.
+        from soaringcoupons import main
+        app = main.make_app()
+
+        self.assertIsNotNone(app)
+
+        self.assertEqual(len(app.router.match_routes), 1)
+
+        route = app.router.match_routes[0]
+        self.assertIs(route.handler, controller.UnconfiguredHandler)
+
+    def test_configured_run(self):
+        # When application is configured, all the handlers are present
+        settings_entity = model.Settings.get_or_insert("test")
+        settings_entity.webtopay_project_id = "test"
+        settings_entity.put()
+        self.assertTrue(settings_entity.is_configured())
+
+        from soaringcoupons import main
+        app = main.make_app()
+
+        self.assertIsNotNone(app)
+        self.assertEqual(len(app.router.match_routes), 11)
