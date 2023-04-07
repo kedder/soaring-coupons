5a8f308912fc8c1feebad65bdbcf813fdd8b1902
Andrey Lebedev <andrey@lebedev.lt>
2023-04-07 21:18:25 +0300

    Migrate storage settings to new format

--- sklandymas/settings.py
+++ sklandymas/settings.py
@@ -124,7 +124,11 @@ LOGGING = {
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = "/static/"
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage"
+    }
+}
 
 WSGI_APPLICATION = "sklandymas.wsgi.application"
 
